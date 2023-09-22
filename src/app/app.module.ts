35e9e899ed117bd6bc9182d142a14222f5315bf4
Erick T <erick_turcios@hotmail.com>
2023-09-22 00:11:49 -0600

    Initial load

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ConfigurationComponent } from './configuration/configuration.component';
+import { GameComponent } from './game/game.component';
+import { HistoryComponent } from './history/history.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConfigurationComponent,
+    GameComponent,
+    HistoryComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      {path: 'main', component: AboutComponent},
+      {path: 'configuration', component: ConfigurationComponent},
+      {path: 'history', component: HistoryComponent},
+      {path: 'play', component: GameComponent},
+      {path: '**', redirectTo: 'main'}, /*Not Found redirect*/
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
